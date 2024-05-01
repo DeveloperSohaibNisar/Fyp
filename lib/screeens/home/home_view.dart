30779a5a5724a197b1e14a4d1501fc58a8f3f906
DeveloperSohaibNisar <developer.sohaibnisar@gmail.com>
2024-05-01 15:07:06 +0500

    moved bottom sheet from tab pages to homePage

--- lib/screeens/home/home_view.dart
+++ lib/screeens/home/home_view.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fyp_application/screeens/home/tabpages/notes/note_view.dart';
 import 'package:fyp_application/screeens/home/tabpages/pdf/pdf_view.dart';
 import 'package:fyp_application/screeens/home/tabpages/recordings/recordings_view.dart';
 import 'package:fyp_application/screeens/settings/settings_view.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fyp_application/screeens/recorder/recorder_view.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
   static const routeName = '/';
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  Widget _bottomSheetButton = const RecordingBottomButton();
+
+  _handleTabSelection(currentIndex) {
+    setState(() {
+      switch (currentIndex) {
+        case 0:
+          setState(() {
+            _bottomSheetButton = const RecordingBottomButton();
+          });
+        case 1:
+          setState(() {
+            _bottomSheetButton = const PdfBottomButton();
+          });
+        case 2:
+          setState(() {
+            _bottomSheetButton = const NoteBottomButton();
+          });
+          break;
+        default:
+          setState(() {
+            _bottomSheetButton = const RecordingBottomButton();
+          });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,15 +104,9 @@ class HomeView extends StatelessWidget {
                   splashBorderRadius: BorderRadius.circular(30),
                   padding: const EdgeInsets.all(8),
                   tabs: const [
-                    Tab(
-                      text: 'Meeting Assitance',
-                    ),
-                    Tab(
-                      text: 'Chat PDF',
-                    ),
-                    Tab(
-                      text: 'My Note',
-                    ),
+                    Tab(text: 'Meeting Assitance'),
+                    Tab(text: 'Chat PDF'),
+                    Tab(text: 'My Note'),
                   ],
                 ),
               ),
@@ -98,6 +128,45 @@ class HomeView extends StatelessWidget {
             ),
           ],
         ),
+        bottomSheet: BottomSheet(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          enableDrag: false,
+          onClosing: () {},
+          builder: (BuildContext context) {
+            final TabController tabController =
+                DefaultTabController.of(context);
+            tabController.addListener(() {
+              if (!tabController.indexIsChanging) {
+                _handleTabSelection(tabController.index);
+              }
+            });
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  'assets/images/vector1.svg',
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(24),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromRGBO(252, 138, 25, 1),
+                        Color.fromRGBO(253, 119, 19, 1),
+                        Color.fromRGBO(254, 96, 12, 1),
+                      ],
+                    ),
+                  ),
+                  child: AnimatedSwitcher(
+                      duration: const Duration(seconds: 1),
+                      child: _bottomSheetButton),
+                ),
+              ],
+            );
+          },
+        ),
         body: const TabBarView(
           children: [
             RecordingView(),
@@ -106,6 +175,148 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class NoteBottomButton extends StatelessWidget {
+  const NoteBottomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton.filledTonal(
+          onPressed: () {},
+          iconSize: 45,
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(Colors.black.withOpacity(.1))),
+          padding: const EdgeInsets.all(15),
+          color: Colors.white,
+          icon: const Icon(
+            Icons.add,
+            shadows: <Shadow>[
+              Shadow(
+                  color: Color.fromRGBO(0, 0, 0, .25),
+                  blurRadius: 4,
+                  offset: Offset(0, 4))
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PdfBottomButton extends StatelessWidget {
+  const PdfBottomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton.filledTonal(
+          onPressed: () {},
+          iconSize: 45,
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(Colors.black.withOpacity(.1))),
+          padding: const EdgeInsets.all(15),
+          color: Colors.white,
+          icon: const Icon(
+            Icons.upload_file,
+            shadows: <Shadow>[
+              Shadow(
+                  color: Color.fromRGBO(0, 0, 0, .25),
+                  blurRadius: 4,
+                  offset: Offset(0, 4))
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RecordingBottomButton extends StatelessWidget {
+  const RecordingBottomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton.filledTonal(
+          onPressed: () async {
+            var status = await Permission.microphone.request();
+            if (!context.mounted) return;
+            switch (status) {
+              case PermissionStatus.permanentlyDenied:
+                var snackBar = SnackBar(
+                  content: const Text('microphone permission required!'),
+                  action: SnackBarAction(
+                    label: 'Open Settings',
+                    onPressed: () => openAppSettings(),
+                  ),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                break;
+              case PermissionStatus.denied:
+                const snackBar = SnackBar(
+                    content: Text('microphone permission not granted'));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                break;
+              case PermissionStatus.granted:
+                Navigator.restorablePushNamed(context, RecorderView.routeName);
+              default:
+            }
+          },
+          iconSize: 45,
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(Colors.black.withOpacity(.1))),
+          padding: const EdgeInsets.all(15),
+          color: Colors.white,
+          icon: const Icon(
+            Icons.mic,
+            shadows: <Shadow>[
+              Shadow(
+                  color: Color.fromRGBO(0, 0, 0, .25),
+                  blurRadius: 4,
+                  offset: Offset(0, 4))
+            ],
+          ),
+        ),
+        const SizedBox(width: 40),
+        IconButton.filledTonal(
+          onPressed: () async {
+            var picked = await FilePicker.platform.pickFiles();
+            if (picked != null) {
+              if (kDebugMode) {
+                print(picked.files.first.name);
+              }
+            }
+          },
+          iconSize: 45,
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(Colors.black.withOpacity(.1))),
+          padding: const EdgeInsets.all(15),
+          color: Colors.white,
+          icon: const Icon(
+            Icons.audio_file,
+            shadows: <Shadow>[
+              Shadow(
+                  color: Color.fromRGBO(0, 0, 0, .25),
+                  blurRadius: 4,
+                  offset: Offset(0, 4))
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
